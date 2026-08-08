17f64b9e4942ea7bd6ec7ca240b4dc1488ef026e
Ayush Saxena <saxenaayush2809@gmail.com>
2026-08-08 08:50:37 +0530

    Day 08: leetcode 12

---
+++ src/leetcode/TwoSumII.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class TwoSumII {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+
+        System.out.println("Enter the elements of the array in sorted manner:");
+        int[] numbers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter target element:");
+        int target = sc.nextInt();
+
+        int left = 0;
+        int right = n - 1;
+        boolean found = false;
+
+        while (left < right) {
+
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                System.out.println("Indices: " + (left + 1) + ", " + (right + 1));
+                found = true;
+                break;
+            }
+            else if (sum < target) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Indices: -1, -1");
+        }
+
+        sc.close();
+    }
+}
+
+
+//Time: O(n)
+//Auxiliary Space: O(1)
